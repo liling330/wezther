b9af858ceee4a351f8911b96988eca1ad50df12d
哩凌 <58362710+liling330@users.noreply.github.com>
2022-04-03 22:41:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
-<script>
+var s = document.createElement('script');
+     s.type = 'text/javascript';
+     s.async = true;
+     s.src = 'https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js';
+     var x = document.getElementsByTagName('script')[0];
+     x.parentNode.insertBefore(s, x);
 let Location = "101280102" //广州番禺  (宝安：101280605  高州：101282002)
 let key = "17997d189efd4e45b849f995679f6205"
 let url = 'https://devapi.qweather.com/v7/weather/now?location='+location+'&key='+key
@@ -16,4 +20,4 @@ $.get("https://devapi.qweather.com/v7/weather/now", {
             let content = date + " 番禺:" + " " + weather + " " +wind + " " +temp + "℃"
             if(content.indexOf( '0' ) == 0){ content = content.substring(1)}
                 window.document.title=content}})
-</script>
+
