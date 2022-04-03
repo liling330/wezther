4f8a62fd1db1520080e7ffdb79d73725448a1904
哩凌 <58362710+liling330@users.noreply.github.com>
2022-04-03 23:48:35 +0800

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,14 +1,14 @@
-
+window.onload = function(){
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src= 'https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js';
-    script.async = true;
+    script.defer = true;
     script.onload = script.onreadystatechange = function() {
     if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete" ) {
         var t_start = Number(new Date());
 while ( t_start + 5000 > Number(new Date()) ) {}
-                let Location = "101280102" //广州番禺  (宝安：101280605  高州：101282002)
+                let Location = "101280102" //广州番禺  (宝安：101280605  高州：101282002)
                 let key = "17997d189efd4e45b849f995679f6205"
                 let url = 'https://devapi.qweather.com/v7/weather/now?location='+location+'&key='+key
                 $.get("https://devapi.qweather.com/v7/weather/now", {
@@ -27,4 +27,4 @@ while ( t_start + 5000 > Number(new Date()) ) {}
                 script.onload = script.onreadystatechange = null;
             }
         };
-        head.appendChild(script);
+        document.body.appendChild(script);}
